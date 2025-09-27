f03456e7794f3ccdb7dafcf53552806d60f351b8
Akshith G <akshithg01@gmail.com>
2025-09-27 06:04:51 +0530

    init db

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const { connectDb } = require("./config/database.js");
+const { User } = require("./models/user.js");
 
 const app = express();
 
 const port = 3000;
+
+app.post("/signup", (req, res) => {
+  try {
+    const userObj = {
+      firstName: "Akshith",
+      lastName: "Gunasheelan",
+      age: 25,
+      gender: "male",
+    };
+
+    const user = new User(userObj);
+    user.save();
+
+    res.status(201).send({
+      message: "User created successfully!",
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: "Something went wrong!",
+      err: err?.message,
+    });
+  }
+});
 
 connectDb()
   .then(() => {

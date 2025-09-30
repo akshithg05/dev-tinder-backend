542c8aa3ae62c077f43a351ce3c65c4ffeaaa772
Akshith G <akshithg01@gmail.com>
2025-09-30 05:43:28 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -6,25 +6,164 @@ const app = express();
 
 const port = 3000;
 
-app.post("/signup", (req, res) => {
-  try {
-    const userObj = {
-      firstName: "Akshith",
-      lastName: "Gunasheelan",
-      age: 25,
-      gender: "male",
-    };
+app.use(express.json());
 
-    const user = new User(userObj);
-    user.save();
+// Create user/ sign up user
+app.post("/signup", async (req, res) => {
+  try {
+    const newUser = req.body;
+
+    const user = new User(newUser);
+    await user.save();
 
     res.status(201).send({
       message: "User created successfully!",
+      data: {
+        user,
+      },
     });
   } catch (err) {
     res.status(400).send({
       message: "Something went wrong!",
       err: err?.message,
+    });
+  }
+});
+
+// Get user by email ID
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.email;
+    const users = await User.find({ emailId: userEmail });
+
+    if (users.length === 0) {
+      res.status(404).send({
+        message: "Users not found",
+      });
+    }
+    res.status(200).send({
+      data: {
+        users: users,
+      },
+    });
+  } catch (err) {
+    res.status(404).send({
+      message: "Not found",
+      err: err.message,
+    });
+  }
+});
+
+// Get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+
+    if (allUsers.length === 0) {
+      res.status(404).send({
+        message: "No users found",
+      });
+    }
+
+    res.status(200).send({
+      data: {
+        count: allUsers.length,
+        users: allUsers,
+      },
+    });
+  } catch (err) {
+    res.status(404).send({
+      message: "Not found",
+      err: err.message,
+    });
+  }
+});
+
+// Get user by ID
+app.get("/user/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id);
+
+    if (!user) {
+      res.status.send(404)({
+        message: "No users found",
+      });
+    }
+
+    res.send(user);
+  } catch (err) {
+    res.status(404).send({
+      message: "Not found",
+      err: err.message,
+    });
+  }
+});
+
+// Delete user by ID
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const objectToDelete = await User.findByIdAndDelete(id);
+
+    if (!objectToDelete) {
+      throw Error(`There is no user with ID ${id}`);
+    }
+    res.status(200).send({
+      message: "User deleted!",
+    });
+  } catch (err) {
+    res.status(404).send({
+      message: "Not found",
+      err: err.message,
+    });
+  }
+});
+
+// Update document by ID
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedBody = req.body;
+    const user = await User.findByIdAndUpdate(id, updatedBody, { returnDocument: "after", runValidators: true });
+
+    if (!user) {
+      throw Error(`There is no user with ID ${id}`);
+    }
+
+    res.send({
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(404).send({
+      message: "Not found",
+      err: err.message,
+    });
+  }
+});
+
+// Update document by email
+app.patch("/user/email/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const updatedBody = req.body;
+    const user = await User.findOneAndUpdate({ emailId: email }, updatedBody, { returnDocument: "after" });
+
+    if (!user) {
+      throw Error(`There is no user  with ID ${id}`);
+    }
+
+    res.send({
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(404).send({
+      message: "Not found",
+      err: err.message,
     });
   }
 });
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,70 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+      maxLength: 16,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 120,
+    },
+
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "others"],
+      },
+    },
+
+    // Below is custom validation
+    // gender: {
+    //   type: String,
+    //   validate: {
+    //     validator: () => {
+    //       if (!["male", "female", "others"].includes(value)) {
+    //         throw Error("The value of gender should be male, female or others");
+    //       }
+    //     },
+    //   },
+    // },
+    about: {
+      type: String,
+      default: "This is the default about me of the user.",
+    },
+    photoURL: {
+      type: String,
+      deafult: "https://www.vhv.rs/dpng/d/256-2569650_men-profile-icon-png-image-free-download-searchpng.png",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 

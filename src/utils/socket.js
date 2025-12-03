5df3a49709f6c117a49dcdbfeab04cacb8bca400
Akshith G <akshithg01@gmail.com>
2025-12-03 07:21:41 +0530

    basic chat and socket connection

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,12 @@
-const http = require("http");
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, toUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, toUserId].sort().join("_"))
+    .digest("hex");
+};
 
 function initializeSocket(server) {
   const io = socket(server, {
@@ -9,8 +16,21 @@ function initializeSocket(server) {
   });
 
   io.on("connection", (socket) => {
-    socket.on("joinChat", () => {});
-    socket.on("sendMessage", () => {});
+    socket.on("joinChat", ({ firstName, userId, toUserId }) => {
+      // We need to create a room with a unique Id now
+      const room = getSecretRoomId(userId, toUserId);
+      console.log(`${firstName} joined room ${room}`);
+      socket.join(room);
+    });
+    socket.on("sendMessage", ({ firstName, userId, toUserId, text }) => {
+      const roomId = getSecretRoomId(userId, toUserId);
+      io.to(roomId).emit("messageReceived", {
+        firstName,
+        userId,
+        toUserId,
+        text,
+      });
+    });
     socket.on("disconnect", () => {});
   });
 }

7e0afe26e9fee47ab462f1dd61163e06999bb6b9
Akshith G <akshithg01@gmail.com>
2025-12-06 11:52:45 +0530

    final backend changes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,8 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
 const { Chat } = require("../models/chat");
+const { ConnectionRequest } = require("../models/connectionRequest");
+const { CONNECTION_REQUEST_STATUS } = require("./constants");
 
 const getSecretRoomId = (userId, toUserId) => {
   return crypto
@@ -17,43 +19,62 @@ function initializeSocket(server) {
   });
 
   io.on("connection", (socket) => {
-    socket.on("joinChat", ({ firstName, userId, toUserId }) => {
+    socket.on("joinChat", ({ firstName, lastName, userId, toUserId }) => {
       // We need to create a room with a unique Id now
       const room = getSecretRoomId(userId, toUserId);
       console.log(`${firstName} joined room ${room}`);
       socket.join(room);
     });
-    socket.on("sendMessage", async ({ firstName, userId, toUserId, text }) => {
-      try {
-        const roomId = getSecretRoomId(userId, toUserId);
+    socket.on(
+      "sendMessage",
+      async ({ firstName, userId, toUserId, text, lastName }) => {
+        try {
+          const roomId = getSecretRoomId(userId, toUserId);
 
-        // Need to save chats here
-
-        let chat = await Chat.findOne({
-          participants: { $all: [userId, toUserId] },
-        });
-
-        if (!chat) {
-          chat = new Chat({
-            participants: [userId, toUserId],
-            messages: [],
+          const areConnected = await ConnectionRequest.findOne({
+            $or: [
+              { fromUserId: userId, toUserId: toUserId },
+              { fromUserId: toUserId, toUserId: userId },
+            ],
+            status: CONNECTION_REQUEST_STATUS.accepted,
           });
+
+          if (!areConnected) {
+            socket.emit("chatError", {
+              message: "You are not connected with this user",
+            });
+            return;
+          }
+
+          // Need to save chats here
+
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, toUserId] },
+          });
+
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, toUserId],
+              messages: [],
+            });
+          }
+
+          chat.messages.push({ senderId: userId, text: text });
+
+          await chat.save();
+          io.to(roomId).emit("messageReceived", {
+            firstName,
+            lastName,
+            userId,
+            toUserId,
+            text,
+            timestamp: new Date(),
+          });
+        } catch (err) {
+          console.log(err);
         }
-
-        chat.messages.push({ senderId: userId, text: text });
-
-        await chat.save();
-        io.to(roomId).emit("messageReceived", {
-          firstName,
-          userId,
-          toUserId,
-          text,
-          timestamp: new Date(),
-        });
-      } catch (err) {
-        console.log(err);
       }
-    });
+    );
     socket.on("disconnect", () => {});
   });
 }
